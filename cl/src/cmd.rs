2f21cfb62c75b750a5da1bd7bcbfe0ad58f16c97
Naoya Furudono <naoyafurudono@gmail.com>
2023-06-29 21:41:09 +0900

    clコマンドを実装

---
+++ cl/src/cmd.rs
@@ -0,0 +1,45 @@
+use std::{fs, path::PathBuf};
+
+use crate::Args;
+
+pub enum Cmd {
+    Copy {
+        from_path: PathBuf,
+        base_name: String,
+    },
+}
+
+impl Cmd {
+    pub fn new(args: &Args) -> Result<Self, String> {
+        let from_path = PathBuf::from(&args.from_path);
+        let base_name = args.base_name.clone();
+        Ok(Cmd::Copy {
+            from_path,
+            base_name,
+        })
+    }
+
+    pub fn run(&self) -> Result<(), String> {
+        match self {
+            Cmd::Copy {
+                from_path,
+                base_name,
+            } => {
+                let mut to = from_path.clone();
+                to.set_file_name(base_name);
+                let from = PathBuf::from(from_path);
+                if !from.exists() {
+                    return Err(format!(
+                        "{} does not exist",
+                        from_path.to_str().ok_or("err")?
+                    ));
+                }
+                if to.exists() {
+                    return Err(format!("{} already exists", to.to_str().ok_or("err")?));
+                }
+                fs::copy(from, to).map_err(|e| e.to_string())?;
+                Ok(())
+            }
+        }
+    }
+}
---
+++ cl/src/main.rs
@@ -0,0 +1,26 @@
+use std::process;
+
+use clap::Parser;
+use cmd::Cmd;
+
+mod cmd;
+
+#[derive(Parser)]
+#[command(name = "cl")]
+#[command(author = "Naoya Furudono <naoyafurudono@gmail.com>")]
+#[command(about = "cl src/deep/path/to/original-file new-file-name\n", long_about = None)]
+pub struct Args {
+    from_path: String,
+    base_name: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    match Cmd::new(&args).and_then(|c| c.run()) {
+        Ok(_) => process::exit(0),
+        Err(e) => {
+            eprintln!("{}", e);
+            process::exit(1)
+        }
+    }
+}
